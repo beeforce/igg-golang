f43ce89ae13fb8c9f909f17699d39c91a0d77e39
Buree Paoton <>
2021-05-20 18:08:57 +0700

    [Edited] map wrokshop

--- basic/main.go
+++ basic/main.go
@@ -2,10 +2,18 @@ package main
 
 import "fmt"
 
+
+type contactInfo struct {
+	email string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName string
+	contact contactInfo
 }
+
 
 func (p person) Print() {
 	fmt.Println(p)
@@ -15,6 +23,10 @@ func main() {
 	bee := person{
 		"Buri",
 		"Paoton",
+		contactInfo{
+			email: "buri@igeargeek.com",
+			zipCode: 50210,
+		},
 	}
 
 	bee.Print()
